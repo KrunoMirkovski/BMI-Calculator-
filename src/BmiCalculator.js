5e82924af9a97d65f300483693f500adbec51a57
KRUNISLAV MIRKOVSKI <kmirkovski@gmial.com>
2024-12-01 02:22:31 +0100

    Add calculation logic based on weight and height

--- src/BmiCalculator.js
+++ src/BmiCalculator.js
@@ -4,28 +4,42 @@ const BmiCalculator = () => {
   const [weight, setWeight] = useState("");
   const [height, setHeight] = useState("");
 
-  return (
-    <div className="container">
-      <div className="inputwrap">
-        <label>
-          Weight (kg):
-          <input
-            type="number"
-            value={weight}
-            onChange={(e) => setWeight(e.target.value)}
-          />
-        </label>
-        <label>
-          Height (cm):
-          <input
-            type="number"
-            value={height}
-            onChange={(e) => setHeight(e.target.value)}
-          />
-        </label>
+  const calculateBMI = () => {
+    if (!weight || !height) {
+      alert("Please enter both weight and height!");
+      return;
+    }
+
+    const heightInMeters = parseFloat(height) / 100;
+    const bmiValue = (
+      parseFloat(weight) /
+      (heightInMeters * heightInMeters)
+    ).toFixed(2);
+    console.log(bmiValue);
+
+    return (
+      <div className="container">
+        <div className="inputwrap">
+          <label>
+            Weight (kg):
+            <input
+              type="number"
+              value={weight}
+              onChange={(e) => setWeight(e.target.value)}
+            />
+          </label>
+          <label>
+            Height (cm):
+            <input
+              type="number"
+              value={height}
+              onChange={(e) => setHeight(e.target.value)}
+            />
+          </label>
+        </div>
       </div>
-    </div>
-  );
+    );
+  };
 };
 
 export default BmiCalculator;
